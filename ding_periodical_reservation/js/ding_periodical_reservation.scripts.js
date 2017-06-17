8201dbfac4e4e72ca407b772fdf255ec33d79e77
Nicolae Grigoriu <ng@inlead.dk>
2017-06-17 16:22:58 +0300

    DBC-1341 - Force user to log in before reservation is made.

--- ding_periodical_reservation/js/ding_periodical_reservation.scripts.js
+++ ding_periodical_reservation/js/ding_periodical_reservation.scripts.js
@@ -3,32 +3,23 @@
  * Script file for the periodical reservations.
  */
 (function ($) {
-  Drupal.extractPeriodicalId = function(ele) {
-    var id_attr = $(ele).attr('id');
-    var id = id_attr.match(/periodical-id-(.+)/);
+  'use strict';
 
-    if (id != null) {
-      return id[1];
-    }
-    else {
-      return false;
-    }
-  }
-
-  trigger_periodical_reservation = function(ajax, response, status) {
+  var trigger_periodical_reservation = function(ajax, response, status) {
     var entity_id = response.data.replace(' ', '%20');
     var forms = $('form');
     var regex = new RegExp(entity_id, 'g');
+
     // Loop through all forms on a page, deeper filtering comes next.
     forms.each(function() {
-      form = $(this);
+      var form = $(this);
       // Wee seek for reservations forms, thus specific form whose item was clicked.
       if (form.attr('id').match(/ding-reservation-reserve-form/g) && form.attr('action').match(regex)) {
         form.hide();
         form.find('.form-submit').mousedown();
       }
     });
-  }
+  };
 
   Drupal.behaviors.ding_periodical_reservation = {
     attach: function (context, settings) {
